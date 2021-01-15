52a6438b6a23e0c37a73a4fa24835c8633a8dbef
simonojok19 <simonojok19@gmail.com>
2021-01-15 14:06:09 +0300

    added the urls

---
+++ courses/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from courses.views import ManageCourseListView, CourseCreateView, CourseUpdateView, CourseDeleteView
+
+urlpatterns = [
+    path('mine/', ManageCourseListView.as_view(), name='manage_course_list'),
+    path('create/', CourseCreateView.as_view(), name='course_create'),
+    path('<pk>/edit/', CourseUpdateView.as_view(), name='course_edit'),
+    path('<pk>/delete/', CourseDeleteView.as_view(), name='course_delete')
+]
