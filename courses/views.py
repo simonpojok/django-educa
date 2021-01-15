a5e8e0e95c363c3c16830050c73d5ee7df203f25
simonojok19 <simonojok19@gmail.com>
2021-01-15 13:11:07 +0300

    added list view

--- courses/views.py
+++ courses/views.py
@@ -14,3 +14,7 @@ def example(request):
 class ManageCourseListView(ListView):
     model = Course
     template_name = 'courses/manage/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
