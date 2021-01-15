3bb1a89c0f7b21e673a6189e6f0233ba0fa600cb
simonojok19 <simonojok19@gmail.com>
2021-01-15 13:09:01 +0300

    added a new template and directory

--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from courses.models import Course
 
 
 def example(request):
     person = User.objects.get(pk=1)
     return HttpResponse(f"Hello world {getattr(person, 'username')}")
+
+
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
