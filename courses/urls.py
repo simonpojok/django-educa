3d69357841d25fb1f3dcba5ac0a884d00b423498
simonojok19 <simonojok19@gmail.com>
2021-01-15 17:12:29 +0300

    created the form module complete

--- courses/urls.py
+++ courses/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from courses.views import ManageCourseListView, CourseCreateView, CourseUpdateView, CourseDeleteView
+from courses.views import ManageCourseListView, CourseCreateView, CourseUpdateView, CourseDeleteView, \
+    CourseModuleUpdateView
 
 urlpatterns = [
     path('mine/', ManageCourseListView.as_view(), name='manage_course_list'),
     path('create/', CourseCreateView.as_view(), name='course_create'),
     path('<pk>/edit/', CourseUpdateView.as_view(), name='course_edit'),
-    path('<pk>/delete/', CourseDeleteView.as_view(), name='course_delete')
+    path('<pk>/delete/', CourseDeleteView.as_view(), name='course_delete'),
+    path('<pk>/module/', CourseModuleUpdateView.as_view(), name='course_module_update')
 ]
