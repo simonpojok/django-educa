02b78b47727ec6f9a1d7120ce54fdfbad4a20edb
simonojok19 <simonojok19@gmail.com>
2021-01-16 09:50:41 +0300

    created the templates

--- educa/settings.py
+++ educa/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'courses.apps.CoursesConfig'
+    'courses.apps.CoursesConfig',
+    'students.apps.StudentsConfig'
 ]
 
 MIDDLEWARE = [
--- students/views.py
+++ students/views.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class StudentRegistrationView(CreateView):
+    template_name = 'students/student/registration.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('student_course_list')
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+        cd = form.cleaned_data
+        user = authenticate(
+            username=cd['username'],
+            password=cd['password1']
+        )
+        login(self.request, user)
+        return result
