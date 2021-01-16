3a54029c88a7930416441adf4d0d7d0f798c256f
simonojok19 <simonojok19@gmail.com>
2021-01-16 11:26:25 +0300

    added the statics

--- educa/urls.py
+++ educa/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 from django.contrib.auth import views as auth_view
+
+from educa import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +28,6 @@ urlpatterns = [
     path('course/', include('courses.urls')),
     path('students/', include('students.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
