7115884ae94a71987e690b5a7b1a79e672fe21e4
simonojok19 <simonojok19@gmail.com>
2021-01-16 10:37:32 +0300

    overridden get_context_data

--- courses/views.py
+++ courses/views.py
@@ -9,6 +9,7 @@ from django.views.generic.base import TemplateResponseMixin, View
 
 from courses.forms import ModuleFormSet
 from courses.models import Course, Module, Content, Subject
+from students.forms import CourseEnrollForm
 
 
 class OwnerMixin(object):
@@ -172,4 +173,12 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(
+            initial={'course': self.object}
+        )
+
+        return context
 
