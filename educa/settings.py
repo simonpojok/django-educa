fa6048aa1dc8bd369d6fe25b6c7c679c44b7e93e
simonojok19 <simonojok19@gmail.com>
2021-01-16 11:24:39 +0300

    added the fix urls settings

--- educa/settings.py
+++ educa/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +125,5 @@ STATIC_URL = '/static/'
 
 
 LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
