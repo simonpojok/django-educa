d81f25c1b1348f1a93bf14f935a57cbb725e629e
simonojok19 <simonojok19@gmail.com>
2021-01-16 08:20:55 +0300

    added url patterns

--- courses/urls.py
+++ courses/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from courses.views import ManageCourseListView, CourseCreateView, CourseUpdateView, CourseDeleteView, \
-    CourseModuleUpdateView, ContentCreateUpdateView, ContentDeleteView, ModuleContentListView
+    CourseModuleUpdateView, ContentCreateUpdateView, ContentDeleteView, ModuleContentListView, CourseListView, \
+    CourseDetailView
 
 urlpatterns = [
+    path('mine/', ManageCourseListView.as_view(), name='manage_course_list'),
     path('mine/', ManageCourseListView.as_view(), name='manage_course_list'),
     path('create/', CourseCreateView.as_view(), name='course_create'),
     path('<pk>/edit/', CourseUpdateView.as_view(), name='course_edit'),
@@ -14,5 +16,8 @@ urlpatterns = [
     path('module/<int:module_id>/content/<model_name>/<id>/',
          ContentCreateUpdateView.as_view(), name='module_content_update'),
     path('content/<int:id>delete/', ContentDeleteView.as_view(), name='module_content_delete'),
-    path('module/<int:module_id>/', ModuleContentListView.as_view(), name='module_content_list')
+    path('module/<int:module_id>/', ModuleContentListView.as_view(), name='module_content_list'),
+    path('', CourseListView.as_view(), name='course_list'),
+    path('subject/<slug:subject>/', CourseListView.as_view(), 'course_list_subject'),
+    path('<slug:slug>/', CourseDetailView.as_view(), name='course_detail')
 ]
