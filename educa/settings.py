91ce18e99918068bfce2ab891166109eeb5903e5
simonojok19 <simonojok19@gmail.com>
2021-01-18 20:37:07 +0300

    added database cache

--- educa/settings.py
+++ educa/settings.py
@@ -127,3 +127,10 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'my_cache_table',
+    }
+}
